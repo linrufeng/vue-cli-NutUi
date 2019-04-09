9be0e7d2968c108a32018005ca0720a273506c19
lilinsen <lilinsen@jd.com>
2019-04-09 15:54:13 +0800

    nut增加引入例子

--- nutui/src/main.js
+++ nutui/src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import { Dialog, Picker} from '@nutui/nutui';
+Dialog.install(Vue);
+Picker.install(Vue);
 Vue.config.productionTip = false
 
 new Vue({
